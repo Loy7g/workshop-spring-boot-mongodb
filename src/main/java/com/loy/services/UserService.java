86abbe2051a69f46067b7314eb2d8e35b39ac362
Maximus <lianthg07@gmail.com>
2025-06-05 14:53:38 -0300

    Getting a user by id

--- src/main/java/com/loy/services/UserService.java
+++ src/main/java/com/loy/services/UserService.java
@@ -3,6 +3,7 @@ package com.loy.services;
 
 import com.loy.domain.User;
 import com.loy.repository.UserRepository;
+import com.loy.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +16,15 @@ public class UserService {
     private UserRepository repo;
 
 
-    public List<User> findAll(){
+    public List<User> findAll() {
         return repo.findAll();
     }
+
+
+    public User findById(String id){
+        return repo.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
 }
+
+
---
+++ src/main/java/com/loy/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package com.loy.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+    public ObjectNotFoundException(String msg) {
+        super(msg);
+    }
+}

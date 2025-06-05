8c55db2c4d7aeb8821c67318a1ec60be00e0feae
Maximus <lianthg07@gmail.com>
2025-06-05 03:03:22 -0300

    Connecting to MongoDB with repository and service

---
+++ src/main/java/com/loy/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.loy.repository;
+
+
+import com.loy.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
---
+++ src/main/java/com/loy/services/UserService.java
@@ -0,0 +1,21 @@
+package com.loy.services;
+
+
+import com.loy.domain.User;
+import com.loy.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+
+    public List<User> findAll(){
+        return repo.findAll();
+    }
+}

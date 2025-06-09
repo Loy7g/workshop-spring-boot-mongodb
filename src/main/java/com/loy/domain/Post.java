e27eeeb87225a748dadd9dc3618d0f7586b8cfd1
Maximus <lianthg07@gmail.com>
2025-06-08 22:54:45 -0300

    Added comments to posts

--- src/main/java/com/loy/domain/Post.java
+++ src/main/java/com/loy/domain/Post.java
@@ -1,11 +1,14 @@
 package com.loy.domain;
 
 import com.loy.dto.AuthorDTO;
+import com.loy.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -17,6 +20,8 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post(){
     }
@@ -70,6 +75,14 @@ public class Post implements Serializable {
         this.author = author;
     }
 
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
@@ -81,6 +94,7 @@ public class Post implements Serializable {
     public int hashCode() {
         return Objects.hashCode(id);
     }
+
 
 
 }
